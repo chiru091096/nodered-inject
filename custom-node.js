36a2221df2ccc952c896ecceb14399aa776c7295
chiru091096 <chiru091096@gmail.com>
2023-08-09 10:38:25 +0530

    Update custom-node.js

--- custom-node.js
+++ custom-node.js
@@ -2,10 +2,13 @@ module.exports = function (RED) {
     function CustomNode(config) {
         RED.nodes.createNode(this, config);
 
+        var node = this;
+
         this.on('input', function (msg) {
-            msg.type = config.type;
-            msg.value = config.value;
-            this.send(msg);
+            // Set msg.type and msg.value based on the node's configuration
+            msg.type = node.type || "";
+            msg.value = node.value || "";
+            node.send(msg);
         });
     }
     RED.nodes.registerType('custom-node', CustomNode);
