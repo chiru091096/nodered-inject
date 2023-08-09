ed4aa886e926675e53b7cf0ca1b9e4b41a7bd37d
chiru091096 <chiru091096@gmail.com>
2023-08-09 10:45:14 +0530

    Update my-inject-node.js

--- my-inject-node.js
+++ my-inject-node.js
@@ -3,8 +3,17 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         var node = this;
         this.on('input', function(msg) {
-            // Inject the predefined payload
-            msg.payload = config.payload;
+            var payload;
+            if (config.type === "string") {
+                payload = config.value;
+            } else if (config.type === "number") {
+                payload = Number(config.value);
+            } else if (config.type === "boolean") {
+                payload = (config.value.toLowerCase() === "true");
+            } else {
+                payload = config.value;
+            }
+            msg.payload = payload;
             node.send(msg);
         });
     }
